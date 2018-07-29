d11679c93e5725ff7243b32c8054cf2ee59ac0f5
Dan Mauk <dmaukdev@gmail.com>
2018-07-29 08:00:36 +0000

    JWT, Passport, Initial Steps

--- compete/routes/index.js
+++ compete/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
+var passport = require('passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 /* GET posts */ 
 router.get('/posts', function(req, res, next) {
@@ -22,6 +24,40 @@ router.post('/posts', function(req, res, next) {
 	
 	res.json(post);
   });
+});
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+	return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password);
+
+  user.save(function (err){
+    if(err) { return next(err); }
+
+	return res.json({token: user.generateJWT()});
+  });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err) { return next(err); }
+
+	if(user){
+      return res.json({token: user.generateJWT()});
+	} else {
+      return res.status(401).json(info);
+	}
+  })(req, res, next);
 });
 
 /* Preload post object if it is a PARAM in a url */
