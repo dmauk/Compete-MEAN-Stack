8fed23395aeb28f2754ac319211ca98b30e5476e
Dan Mauk <dmaukdev@gmail.com>
2018-08-02 06:27:32 +0000

    More styling!

--- compete/app.js
+++ compete/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var octicon = require('octicon');
 var passport = require('passport');
 
 require('./models/Posts');
