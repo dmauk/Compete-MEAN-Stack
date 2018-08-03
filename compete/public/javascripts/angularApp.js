04b2c0887304721b6d579d3da1f82091f8032406
Dan Mauk <dmaukdev@gmail.com>
2018-08-03 06:26:37 +0000

    bad url handling

--- compete/public/javascripts/angularApp.js
+++ compete/public/javascripts/angularApp.js
@@ -71,6 +71,11 @@ app.config([
 	$urlRouterProvider.otherwise('home');
 }]);
 
+function is_url(str){
+  regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  if (regexp.test(str)) { return true;}
+  return false;
+}
 
 app.controller('MainCtrl', [
 '$scope',
@@ -82,13 +87,20 @@ function($scope, auth, posts){
 
   $scope.currentUser = auth.currentUser;
   $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') {return}
+    if(!$scope.title || $scope.title === '') {return;}
+    if($scope.link !== undefined){
+      if(!is_url($scope.link)) {
+        $scope.error="Invalid URL";
+        return;
+      }
+    }
     posts.create({
 	  title: $scope.title,
 	  link: $scope.link,
           description: $scope.description
 	});
 	$scope.title = '';
+        $scope.error = '';
 	$scope.link = '';
         $scope.description = '';
   };
